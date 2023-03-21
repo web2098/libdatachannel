51eeedaac7878822f35e99e7334a56def42fbc18
Eric Gressman <eric.gressman@hp.com>
2023-03-21 09:25:46 -0600

    Adding support for proxy transport to WSTransport

--- src/impl/wstransport.cpp
+++ src/impl/wstransport.cpp
@@ -42,7 +42,7 @@ using std::to_integer;
 using std::to_string;
 using std::chrono::system_clock;
 
-WsTransport::WsTransport(variant<shared_ptr<TcpTransport>, shared_ptr<TlsTransport>> lower,
+WsTransport::WsTransport(variant<shared_ptr<TcpTransport>, shared_ptr<HttpProxyTransport>, shared_ptr<TlsTransport>> lower,
                          shared_ptr<WsHandshake> handshake, int maxOutstandingPings,
                          message_callback recvCallback, state_callback stateCallback)
     : Transport(std::visit([](auto l) { return std::static_pointer_cast<Transport>(l); }, lower),
@@ -50,6 +50,7 @@ WsTransport::WsTransport(variant<shared_ptr<TcpTransport>, shared_ptr<TlsTranspo
       mHandshake(std::move(handshake)),
       mIsClient(
           std::visit(rtc::overloaded{[](shared_ptr<TcpTransport> l) { return l->isActive(); },
+		  							 [](shared_ptr<HttpProxyTransport> l) { return l->isActive(); },
                                      [](shared_ptr<TlsTransport> l) { return l->isClient(); }},
                      lower)),
       mMaxOutstandingPings(maxOutstandingPings) {
